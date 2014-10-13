f87fcf27d13c78e31f31d6c0c116f0941099eb4a
piyusha89 <piyush.agarwal89@gmail.com>
2014-10-13 12:28:21 +0530

    database update

--- lms.sql
+++ lms.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 08, 2013 at 12:06 PM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+-- Host: 127.0.0.1
+-- Generation Time: Oct 13, 2014 at 08:57 AM
+-- Server version: 5.5.36
+-- PHP Version: 5.4.27
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,28 +17,157 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `party_pundits`
+-- Database: `lms`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Table structure for table `course`
 --
 
-CREATE TABLE IF NOT EXISTS `login` (
+CREATE TABLE IF NOT EXISTS `course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`id`, `name`) VALUES
+(1, 'java'),
+(2, '.net');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE IF NOT EXISTS `exam` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subjectId` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `exam`
+--
+
+INSERT INTO `exam` (`id`, `subjectId`, `name`) VALUES
+(1, 2, 'first'),
+(2, 1, 'second');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `examquestion`
+--
+
+CREATE TABLE IF NOT EXISTS `examquestion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `examId` int(11) NOT NULL,
+  `questionId` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE IF NOT EXISTS `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `que` text NOT NULL,
+  `type` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`id`, `que`, `type`) VALUES
+(1, 'kjkjhkjhkj', 'Easy'),
+(2, 'jhkhkjh', 'Easy'),
+(3, 'lkjlkjlkj', 'Easy'),
+(4, 'kjhkjhkjh', 'Easy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE IF NOT EXISTS `student` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
--- Dumping data for table `login`
+-- Dumping data for table `student`
 --
 
-INSERT INTO `login` (`id`, `username`, `password`, `email`) VALUES
-(1, 'piyush', 'fWi3LKEclDBGhoY2', 'piyush.agarwal89@gmail.com'),
-(2, 'praveen', '123', 'praveen@gmail.com'),
-(3, 'neeshu', '123', 'neeshu@gmail.com');
+INSERT INTO `student` (`id`, `username`, `password`, `email`, `firstname`, `lastname`, `mobile`) VALUES
+(1, 'piyush', '1111', 'piyush.agarwal89@gmail.com', '', '', ''),
+(2, 'praveen', '123', 'praveen@gmail.com', '', '', ''),
+(3, 'neeshu', '123', 'neeshu@gmail.com', '', '', ''),
+(4, 'shobhit', '1111', '', '', '', ''),
+(5, 'shreya', '1111', '', '', '', ''),
+(6, 'hkjh', 'hkjhkh', '', '', '', ''),
+(7, 'vipin', '1234', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE IF NOT EXISTS `subject` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `courseId` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`id`, `courseId`, `name`) VALUES
+(1, 1, 'core'),
+(2, 1, 'advance'),
+(3, 2, 'c#'),
+(4, 2, 'vb'),
+(5, 2, 'asp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user1`
+--
+
+CREATE TABLE IF NOT EXISTS `user1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) NOT NULL,
+  `fastName` varchar(40) NOT NULL,
+  `lastName` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
